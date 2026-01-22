43ecab3844fd3c59fde4b08ea9dd2c3aec6615fc
Elena <163721747+EleVi1@users.noreply.github.com>
2026-01-22 19:45:08 +0100

    Retrive hackathon poster in public page (#27)
    * Fix: access to FAQ page content
    
    * Fix: merge Dashboard.vue for user and expert
    
    * Fix: page public
    
    * Fix: edit eligibleTeams for users when they have no subjects
    
    * Fix: changed description for hackathon poster
    
    * Fix: add redirections for partners website url
    
    * Fix: add fixed height for footer
    
    * Fix: add reset phases button on hackathon management
    
    * Feat: add feedback task and evaluation task
    
    * Fix: enable to assign several teams to jury and mentor
    
    * Fix: retrieve hackathon poster in public page

--- src/pages/public/Home.vue
+++ src/pages/public/Home.vue
@@ -1,11 +1,59 @@
 <script setup lang="ts">
-  import HackathonExample from '@/assets/images/hackathon-example.jpg'
   import Partners from '@/components/public/Partners.vue'
   import Announcements from '@/components/common/Announcements.vue'
   import Info from '@/components/public/Info.vue'
   import { useI18n } from 'vue-i18n'
+  import { watch, ref } from 'vue'
+  import { useConfiguration } from '@/composables/useConfiguration'
+  import { ConfigurationKey } from '@/utils/configuration/configurationKey'
+  import { HackathonMediaDTO } from '@/types/config'
+  import { defaultConfigurations } from '@/utils/configuration/defaultConfiguration'
+  import { S3BucketService } from '@/services/s3BucketService'
 
   const { t } = useI18n()
+
+  const { configuration: mediaConfig, loading: mediaLoading } = useConfiguration(
+    ConfigurationKey.MEDIA
+  )
+
+  const mediaSettings = ref<HackathonMediaDTO>({ ...defaultConfigurations[ConfigurationKey.MEDIA] })
+  const bannerFileInput = ref<File | null>(null)
+
+  // Image par défaut si rien n'est trouvé
+  const defaultBanner =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/NZ_default_banner.jpg/640px-NZ_default_banner.jpg'
+  const bannerPicture = ref(defaultBanner)
+
+  const fetchMediaUrls = async () => {
+    // On vérifie dans mediaSettings (qui sera mis à jour par le watch)
+    if (mediaSettings.value?.bannerPictureId) {
+      try {
+        const response = await S3BucketService.getFileUrlPublic(
+          'public_files',
+          mediaSettings.value.bannerPictureId
+        )
+        bannerPicture.value = response.url
+      } catch (err) {
+        console.error('Error fetching hackathon poster:', err)
+        bannerPicture.value = defaultBanner
+      }
+    }
+  }
+
+  watch(
+    mediaConfig,
+    (newConfig) => {
+      if (newConfig) {
+        mediaSettings.value = {
+          ...defaultConfigurations[ConfigurationKey.MEDIA],
+          ...(newConfig.value as HackathonMediaDTO),
+        }
+        fetchMediaUrls()
+      }
+    },
+    { immediate: true }
+  )
+  const getPreviewUrl = (file: File) => URL.createObjectURL(file)
 </script>
 
 <template>
@@ -13,7 +61,7 @@
     <div class="flex w-full gap-2 mb-6 min-h-[300px]">
       <div class="w-1/4 flex items-start justify-center overflow-hidden" style="height: 100%">
         <img
-          :src="HackathonExample"
+          :src="bannerFileInput ? getPreviewUrl(bannerFileInput) : bannerPicture"
           alt="Hackathon Poster"
           class="h-auto max-h-[500px] w-auto object-contain object-top rounded"
         />
@@ -33,7 +81,7 @@
         <h2 class="text-3xl font-semibold mt-2 mb-3">
           {{ t('announcements.lastAnnouncements') }}
         </h2>
-        <Announcements :items-per-page="5" :is-public="true"/>
+        <Announcements :items-per-page="5" :is-public="true" />
       </div>
     </div>
   </v-container>
